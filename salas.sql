021b36dad4294e5d979a2af650bb809c8ddc291a
Jonas Duarte <48072892+duzzsys@users.noreply.github.com>
2019-07-31 21:50:02 -0300

    Estrutura do Banco de Dados Atualizada

--- salas.sql
+++ salas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 31-Jul-2019 às 04:57
+-- Generation Time: 02-Ago-2019 às 02:55
 -- Versão do servidor: 10.1.37-MariaDB
 -- versão do PHP: 7.3.1
 
@@ -25,20 +25,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `sala 1`
+-- Estrutura da tabela `capela 1 avelar`
 --
 
-CREATE TABLE `sala 1` (
+CREATE TABLE `capela 1 avelar` (
   `Pessoa` text NOT NULL,
-  `Horario` time NOT NULL
+  `Horario` text NOT NULL,
+  `Sepultamento` text NOT NULL,
+  `Cemiterio` text NOT NULL,
+  `Status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Extraindo dados da tabela `sala 1`
+-- Extraindo dados da tabela `capela 1 avelar`
 --
 
-INSERT INTO `sala 1` (`Pessoa`, `Horario`) VALUES
-('Fulaninho de Tal', '00:02:00');
+INSERT INTO `capela 1 avelar` (`Pessoa`, `Horario`, `Sepultamento`, `Cemiterio`, `Status`) VALUES
+('Fulaninho de Tal', '12:31', 'asdasda', 'asdasdas', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `capela 2 avelar`
+--
+
+CREATE TABLE `capela 2 avelar` (
+  `Pessoa` text NOT NULL,
+  `Horario` text NOT NULL,
+  `Sepultamento` text NOT NULL,
+  `Cemiterio` text NOT NULL,
+  `Status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `capela 2 avelar`
+--
+
+INSERT INTO `capela 2 avelar` (`Pessoa`, `Horario`, `Sepultamento`, `Cemiterio`, `Status`) VALUES
+('Jose Cirino de Jesus', '12:45', '02/08/2019', 'Cemitério Municipal', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `capela 3 avelar`
+--
+
+CREATE TABLE `capela 3 avelar` (
+  `Pessoa` text NOT NULL,
+  `Horario` text NOT NULL,
+  `Sepultamento` text NOT NULL,
+  `Cemiterio` text NOT NULL,
+  `Status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
